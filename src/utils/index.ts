f9ec24f8881915350c13f9783e91f9460a2e5e61
Tausif Shaikh <shaikhtausif089@gmail.com>
2025-11-24 18:22:28 +0530

    remove cookie domain

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,11 @@
 import { CookieOptions } from "express"
 import { JwtPayload } from "jsonwebtoken";
-const COOKIE_DOMAIN = 'task-pilot-frontend-aoax.onrender.com';
 
 export const accessTokenCookieOptions: CookieOptions = {
     httpOnly : false,
     secure : true, // Must be true for SameSite='None' in production
     maxAge: 3600000 * 24, // 1 day
     sameSite: 'none' ,// Must be 'None' for cross-site cookies
-    domain : COOKIE_DOMAIN
 };
 
 export const refreshTokenCookieOptions: CookieOptions = {
@@ -15,8 +13,6 @@ export const refreshTokenCookieOptions: CookieOptions = {
     secure : true, // Must be true
     maxAge: 3600000 * 24 * 7, // 7 days
     sameSite: 'none' ,// Must be 'None'
-    domain : COOKIE_DOMAIN
-
 };
 
 
